73c87800722c30537f4ce4da3fd89a300f59678c
garritfra <garritfranke@gmail.com>
2018-11-08 15:25:21 +0100

    Add UI

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,14 +1,46 @@
 import * as React from "react";
+import BetManager from "../core/BetManager";
 
 export default class App extends React.Component<any, any> {
   props: any;
+  state: any;
+  betManager: BetManager;
 
   constructor(props: any) {
     super(props);
     this.props = props;
+    this.betManager = new BetManager(100, 2);
+    this.state = {
+      deposit: this.betManager.getDeposit(),
+      currentBet: this.betManager.getCurrentBet()
+    };
+  }
+
+  handleWon(event: Event) {
+    this.betManager.roundWon();
+    this.update();
+  }
+
+  handleLost() {
+    this.betManager.roundLost();
+    this.update();
+  }
+
+  update() {
+    this.setState({
+      deposit: this.betManager.getDeposit(),
+      currentBet: this.betManager.getCurrentBet()
+    });
   }
 
   render() {
-    return <h1>Hello from App</h1>;
+    return (
+      <div>
+        <h1>Deposit: {this.state.deposit}</h1>
+        <h1>Current Bet: {this.state.currentBet}</h1>
+        <button onClick={this.handleWon.bind(this)}>Round Won</button>
+        <button onClick={this.handleLost.bind(this)}>Round Lost</button>
+      </div>
+    );
   }
 }
