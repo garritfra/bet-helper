839dc135dae7cb961eb8c14cec21ce407172fb73
garritfra <garritfranke@gmail.com>
2018-11-08 14:28:37 +0100

    Fix demo app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import BetManager from "./BetManager";
 
-const betManager = new BetManager(100, 2);
+const betManager = new BetManager(100, 5);
+let outOfMoney = false;
 
 const printInfo = () => {
   console.log("Deposit: " + betManager.getDeposit());
@@ -14,10 +15,13 @@ const hasWon = () => Math.random() >= 0.5;
 printInfo();
 
 for (let i = 0; i < 100; i++) {
-  if (betManager.getDeposit() >= 0) {
-    hasWon() ? betManager.roundWon() : betManager.roundLost();
-    printInfo();
-  } else {
-    console.log("Out of money!");
+  if (!outOfMoney) {
+    if (betManager.getDeposit() >= 0) {
+      hasWon() ? betManager.roundWon() : betManager.roundLost();
+      printInfo();
+    } else {
+      outOfMoney = true;
+      console.log("Out of money!");
+    }
   }
 }
